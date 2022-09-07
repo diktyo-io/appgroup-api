cf80f637002cdd6a1ab13c0809c3f8ec5485f762
Jose Santos <josepedro.pereiradossantos@ugent.be>
2022-09-07 16:14:00 +0200

    namespace addition

--- pkg/apis/appgroup/v1alpha1/types.go
+++ pkg/apis/appgroup/v1alpha1/types.go
@@ -25,7 +25,6 @@ const (
 )
 
 // +genclient
-// +genclient:nonNamespaced
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:resource:scope=Cluster,shortName=ag
 
