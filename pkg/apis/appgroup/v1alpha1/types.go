b94bd6b50fa9e2b25753a9c7a1ea7cf2abfb35d3
Jose Santos <josepedro.pereiradossantos@ugent.be>
2022-12-08 10:13:57 +0100

    api modification

--- pkg/apis/appgroup/v1alpha1/types.go
+++ pkg/apis/appgroup/v1alpha1/types.go
@@ -38,7 +38,7 @@ type AppGroup struct {
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
 	// AppGroupSpec defines the number of Pods and which Pods belong to the group.
-	Spec AppGroupSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	Spec AppGroupSpec `json:"spec" protobuf:"bytes,2,opt,name=spec"`
 
 	// AppGroupStatus defines the observed use.
 	// +optional
@@ -49,22 +49,28 @@ type AppGroup struct {
 // +protobuf=true
 type AppGroupSpec struct {
 	// NumMembers defines the number of Pods belonging to the App Group
-	NumMembers int32 `json:"numMembers,omitempty" protobuf:"bytes,1,opt,name=numMembers"`
+	// +kubebuilder:validation:Minimum=1
+	// +required
+	NumMembers int32 `json:"numMembers" protobuf:"bytes,1,opt,name=numMembers"`
 
 	// The preferred Topology Sorting Algorithm
-	TopologySortingAlgorithm string `json:"topologySortingAlgorithm,omitempty" protobuf:"bytes,2,opt,name=topologySortingAlgorithm"`
+	// +required
+	TopologySortingAlgorithm string `json:"topologySortingAlgorithm" protobuf:"bytes,2,opt,name=topologySortingAlgorithm"`
 
 	// Workloads defines the workloads belonging to the group
-	Workloads AppGroupWorkloadList `json:"workloads,omitempty" protobuf:"bytes,3,rep,name=workloads, casttype=AppGroupWorkloadList"`
+	// +required
+	Workloads AppGroupWorkloadList `json:"workloads" protobuf:"bytes,3,rep,name=workloads, casttype=AppGroupWorkloadList"`
 }
 
 // AppGroupWorkload represents the Workloads belonging to the App Group.
 // +protobuf=true
 type AppGroupWorkload struct {
 	// Workload reference Info.
-	Workload AppGroupWorkloadInfo `json:"workload,omitempty" protobuf:"bytes,1,opt,name=workload, casttype=AppGroupWorkloadInfo"`
+	// +required
+	Workload AppGroupWorkloadInfo `json:"workload" protobuf:"bytes,1,opt,name=workload, casttype=AppGroupWorkloadInfo"`
 
 	// Dependencies of the Workload.
+	// +optional
 	Dependencies DependenciesList `json:"dependencies,omitempty" protobuf:"bytes,2,opt,name=dependencies, casttype=DependenciesList"`
 }
 
@@ -72,13 +78,16 @@ type AppGroupWorkload struct {
 // +protobuf=true
 type AppGroupWorkloadInfo struct {
 	// Kind of the workload, info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds"
-	Kind string `json:"kind,omitempty" protobuf:"bytes,1,opt,name=kind"`
+	// +required
+	Kind string `json:"kind" protobuf:"bytes,1,opt,name=kind"`
 
 	// Name represents the workload, info: http://kubernetes.io/docs/user-guide/identifiers#names
-	Name string `json:"name,omitempty" protobuf:"bytes,2,opt,name=name"`
+	// +required
+	Name string `json:"name" protobuf:"bytes,2,opt,name=name"`
 
 	// Selector defines how to find Pods related to the Workload (key = workload). (e.g., workload=w1)
-	Selector string `json:"selector,omitempty" protobuf:"bytes,3,opt,name=selector"`
+	// +required
+	Selector string `json:"selector" protobuf:"bytes,3,opt,name=selector"`
 
 	// ApiVersion defines the versioned schema of an object.
 	//+optional
@@ -104,7 +113,9 @@ type DependenciesInfo struct {
 	MinBandwidth resource.Quantity `json:"minBandwidth,omitempty" protobuf:"bytes,2,opt,name=minBandwidth"`
 
 	// Max Network Cost between workloads
-	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Default=0
+	// +kubebuilder:validation:Maximum=10000
 	MaxNetworkCost int64 `json:"maxNetworkCost,omitempty" protobuf:"bytes,3,opt,name=maxNetworkCost"`
 }
 
@@ -117,15 +128,19 @@ type DependenciesList []DependenciesInfo
 type AppGroupStatus struct {
 	// The number of actively running workloads (e.g., number of pods).
 	// +optional
+	// +kubebuilder:validation:Minimum=0
 	RunningWorkloads int32 `json:"runningWorkloads,omitempty" protobuf:"bytes,1,opt,name=runningWorkloads"`
 
 	// ScheduleStartTime of the group
+	// +optional
 	ScheduleStartTime metav1.Time `json:"scheduleStartTime,omitempty" protobuf:"bytes,2,opt,name=scheduleStartTime"`
 
 	// TopologyCalculationTime of the group
+	// +optional
 	TopologyCalculationTime metav1.Time `json:"topologyCalculationTime,omitempty" protobuf:"bytes,3,opt,name=topologyCalculationTime"`
 
 	// Topology order for TopSort plugin (QueueSort)
+	// +optional
 	TopologyOrder AppGroupTopologyList `json:"topologyOrder,omitempty" protobuf:"bytes,4,rep,name=topologyOrder,casttype=TopologyList"`
 }
 
